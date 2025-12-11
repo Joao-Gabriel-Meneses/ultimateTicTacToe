d72ebe8c08ba7f485c8d04a33056a0c97b48ea12
Felipe <feduque678@gmail.com>
2025-12-11 11:29:16 -0300

    primeiro commit com a interface gráfica com tela de inicio e botões

---
+++ estrutura.hpp
@@ -0,0 +1,26 @@
+#ifndef ESTRUTURA_HPP
+#define ESTRUTURA_HPP
+
+#include <stdio.h>
+
+#define LINHA 3
+#define COLUNA 3
+#define PROFUNDIDADE 9
+
+enum class BoardMark : char {
+    EMPTY = '-',
+    XMARK = 'X',
+    OMARK = 'O'
+};
+
+//Funções:
+bool detect_victory(BoardMark matrix[PROFUNDIDADE][LINHA][COLUNA], int index_played);
+void init(BoardMark matrix[PROFUNDIDADE][LINHA][COLUNA]);
+//void play(BoardMark matrix[PROFUNDIDADE][LINHA][COLUNA], int max_plays);
+//void print(BoardMark matrix[PROFUNDIDADE][LINHA][COLUNA]);
+void drawUltimateBoard();
+void drawMarks();
+void handleMouseInput();
+
+
+#endif 
